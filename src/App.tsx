e9c1fc98839a863416d546244605c34266bdca7d
AlexLig <ligkalex@gmail.com>
2019-04-18 17:34:26 +0300

    time picker and date fns

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
-import { EmployeeForm } from './components/EmployeeForm';
+import EmployeeForm from './components/EmployeeForm';
+import { MuiPickersUtilsProvider } from 'material-ui-pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 class App extends Component {
   render() {
     return (
       <>
         <CssBaseline />
-        <div className="App">
-          <EmployeeForm />
-        </div>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <div className="App">
+            <EmployeeForm />
+          </div>
+        </MuiPickersUtilsProvider>
       </>
     );
   }
--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -1,20 +1,81 @@
-import React, { FC } from 'react';
-import PropTypes from 'prop-types';
+import React, { FC, useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import PersonIcon from '@material-ui/icons/Person';
+import WorkIcon from '@material-ui/icons/Work';
 import ButtonsAppBar from './ButtonsAppBar';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { TimePicker } from 'material-ui-pickers';
+import { format } from 'date-fns';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    width: '100%',
+  },
+  // margin: {
+  //   marginTop: 20,
+  // },
+};
 
-
-export const EmployeeForm: FC = props => {
+interface IProps {
+  classes: any;
+}
+const EmployeeForm: FC<IProps> = props => {
+  const { classes } = props;
+  const [selectedDate, handleDateChange] = useState(new Date());
   return (
     <div>
-      <ButtonsAppBar title='Αποθήκευση υπαλλήλου' />
+      <ButtonsAppBar title="Αποθήκευση υπαλλήλου" />
+      <TextField
+        className={classes.margin}
+        label="Ονοματεπώνυμο"
+        fullWidth
+        margin="normal"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <PersonIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        className={classes.margin}
+        label="ΑΦΜ"
+        fullWidth
+        margin="normal"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <WorkIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TimePicker
+        showTodayButton
+        todayLabel="now"
+        label="Έναρξη"
+        ampm={false}
+        value={selectedDate}
+        minutesStep={5}
+        onChange={handleDateChange}
+      />
+      <TimePicker
+        showTodayButton
+        todayLabel="now"
+        label="Έναρξη"
+        ampm={false}
+        value={selectedDate}
+        minutesStep={5}
+        onChange={handleDateChange}
+      />
+      {/* {format(selectedDate, 'HHmm')} */}
     </div>
   );
 };
+
+export default withStyles(styles)(EmployeeForm);
