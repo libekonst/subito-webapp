960fc5f3836563c8d5f95faf92e5af055295ca65
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-04-21 05:03:39 +0300

    Update styles.

--- src/screens/E8Form/BottomMessageTile.tsx
+++ src/screens/E8Form/BottomMessageTile.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
 import SmsIcon from '@material-ui/icons/Sms';
+import FileCopyIcon from '@material-ui/icons/FileCopyOutlined';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
 
 interface IProps extends WithStyles<typeof styles> {
@@ -21,13 +24,15 @@ function BottomMessageTile(props: IProps) {
 
   return (
     <Paper className={props.classes.messageTile}>
-      <Chip
-        label={props.message}
-        className={props.classes.messageTileText}
+      <Button
         onClick={navigator.clipboard && copyToClipboard}
-      />
+        className={props.classes.messageContainer}
+      >
+        <Typography noWrap>{props.message}</Typography>
+        <FileCopyIcon color="primary" fontSize="small" />
+      </Button>
       <IconButton
-        className={props.classes.messageTileIconButton}
+        className={props.classes.sendButton}
         component={aProps => <a href={`sms:12345?body=${props.message}`} {...aProps} />}
       >
         <SmsIcon color="primary" />
@@ -51,18 +56,23 @@ const styles = (theme: Theme) =>
 
       // Flex
       display: 'flex',
-      justifyContent: 'space-between',
+      justifyContent: 'stretch',
       alignItems: 'center',
     },
-    messageTileIconButton: {
+    sendButton: {
       margin: theme.spacing.unit / 2,
     },
-    messageTileText: {
-      fontSize: '1rem',
+    messageContainer: {
+      // Flex
+      width: '100%',
       display: 'flex',
-      justifyContent: 'flex-start',
-      flex: 1,
-      overflow: 'hidden',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+
+      // Styles
+      padding: theme.spacing.unit,
+      backgroundColor: theme.palette.grey[300],
+      borderRadius: 50,
     },
   });
 
