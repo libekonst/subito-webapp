524d0277656e864e76669aa28e820f7d3de476eb
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-04-21 22:12:12 +0300

    Updates

---
+++ precache-manifest.74ae2459e3af01ec11e67d7cddd57be0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6785ed53c06f9b6484",
+    "url": "/subito-webapp/static/js/runtime~main.f9f43a59.js"
+  },
+  {
+    "revision": "6da068263ba9c728d360",
+    "url": "/subito-webapp/static/js/main.1972b3ba.chunk.js"
+  },
+  {
+    "revision": "6f06914f2d0693aabd6a",
+    "url": "/subito-webapp/static/js/2.0f837785.chunk.js"
+  },
+  {
+    "revision": "6da068263ba9c728d360",
+    "url": "/subito-webapp/static/css/main.94ff7c41.chunk.css"
+  },
+  {
+    "revision": "757d7dd862b80d175061d21cfb04501d",
+    "url": "/subito-webapp/index.html"
+  }
+];
