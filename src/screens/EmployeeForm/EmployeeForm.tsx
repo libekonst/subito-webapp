bd9fbc9df94852c4607bd003c1318392067a8bc1
AlexLig <ligkalex@gmail.com>
2019-04-23 22:25:27 +0300

    Update EmployeeForm.tsx

--- src/screens/EmployeeForm/EmployeeForm.tsx
+++ src/screens/EmployeeForm/EmployeeForm.tsx
@@ -2,16 +2,19 @@ import React, { FC, useState } from 'react';
 import { withStyles, Theme, createStyles, WithStyles } from '@material-ui/core/styles';
 import WorkIcon from '@material-ui/icons/Work';
 import TextField from '@material-ui/core/TextField';
-import { TimePicker } from 'material-ui-pickers';
-import { format, addHours, setHours, setMinutes } from 'date-fns';
+import { addHours, setHours, setMinutes } from 'date-fns';
 import { stickyTopWithAppbar } from '../../styles/mixins';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ArrowFwDIcon from '@material-ui/icons/ArrowForward';
 import WorkHourPicker from '../../components/WorkHourPicker';
+import validateOnChange, {
+  shouldType,
+} from '../../FormsValidations/employeeFormValidations';
+import { IEmployee } from '../../interfaces';
+import { IEmployeeErrors } from '../../interfaces/IEmployeeErrors';
 
 interface IProps extends WithStyles<typeof styles> {}
 const EmployeeForm: FC<IProps> = props => {
@@ -20,12 +23,35 @@ const EmployeeForm: FC<IProps> = props => {
 
   const date: Date = setMinutes(setHours(new Date(), 8), 0);
 
+  const [errors, setErrors] = useState<IEmployeeErrors>({
+    name: '',
+    vat: '',
+    workStart: '',
+    workFinish: '',
+  });
+  const [values, setValues] = useState({ name: '', vat: '' });
   const [workStart, setWorkStart] = useState(date);
-  const [workFinish, setWorkFinish] = useState(addHours(workStart, 8));
+  const [workFinish, setWorkFinish] = useState(addHours(date, 8));
+  
+  // const [isSubmitting, setIsSubmitting] = useState(false);
 
-  function handleWorkStart(e:any){
-    
-  }
+  // useEffect(() => {
+  //   if (Object.keys(errors).length === 0 && isSubmitting) {
+  //     // callback();
+  //   }
+  // }, [errors]);
+
+  // const handleSubmit = (event: any) => {
+  //   if (event) event.preventDefault();
+  //   setErrors(validateOnSubmit(values));
+  //   setIsSubmitting(true);
+  // };
+
+  const handleChange = (valueName: string) => (event: any) => {
+    // event.persist();
+    // if (!shouldType(event.target.value, valueName)) return;
+    setValues(values => ({ ...values, [valueName]: event.currentTarget.value }));
+  };
   return (
     <>
       <List
@@ -42,7 +68,10 @@ const EmployeeForm: FC<IProps> = props => {
           </ListItemIcon>
           <TextField
             variant={variant}
-            label="Ονοματεπώνυμο"
+            label={errors.name || 'Ονοματεπώνυμο'}
+            error={!!errors.name}
+            value={values.name}
+            onChange={handleChange('name')}
             className={classes.textField}
           />
         </ListItem>
@@ -53,7 +82,10 @@ const EmployeeForm: FC<IProps> = props => {
           <TextField
             variant={variant}
             type="tel"
-            label="ΑΦΜ"
+            label={errors.vat || 'ΑΦΜ'}
+            error={!!errors.vat}
+            value={values.vat}
+            onChange={handleChange('vat')}
             className={classes.textField}
           />
         </ListItem>
@@ -68,6 +100,8 @@ const EmployeeForm: FC<IProps> = props => {
       >
         <ListItem key="WorkHour">
           <WorkHourPicker
+            labelStart="Ώρα Έναρξης"
+            labelFinish="Ώρα Λήξης"
             valueStart={workStart}
             valueFinish={workFinish}
             onChangeStart={setWorkStart}
