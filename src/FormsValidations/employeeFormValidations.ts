2cecdede21c66461d48e24fa3681e2d6622fe927
AlexLig <ligkalex@gmail.com>
2019-04-24 17:06:28 +0300

    Update employeeFormValidations.ts

--- src/FormsValidations/employeeFormValidations.ts
+++ src/FormsValidations/employeeFormValidations.ts
@@ -17,11 +17,16 @@ export default function validateOnChange(values: IEmployee) {
   return errors;
 }
 export const shouldType = (value: string, valueName: string) =>
-  valueName === 'vat' && value.length > 9;
+  valueName === 'vat' && value.length > 9 ? false : true;
 
 export function validateOnSubmit(values: IEmployee) {
   let errors: IEmployeeErrors = { name: '', vat: '', workStart: '', workFinish: '' };
-  if (!isNumeric.test(values.vat)) errors.vat = 'Ο Αφμ αποταιλείται μόνο απο αριθμούς';
-  if (values.vat.length !== 9) errors.vat = 'Ο Αφμ αποταιλείται απο 9 αριθμούς';
+  if (!isNumeric.test(values.vat)) errors.vat = 'Ο Αφμ αποταιλείται μόνο απο αριθμούς.';
+  if (!values.vat) errors.vat = 'Παρακαλώ προσθέσθε ΑΦΜ.';
+  if (values.vat && values.vat.length !== 9)
+    errors.vat = 'Ο Αφμ αποταιλείται απο 9 αριθμούς.';
+  if (!values.name) errors.name = 'Παρακαλώ προσθέσθε ονοματεπώνυμο.';
+  if (values.name && values.name.length > 255)
+    errors.name = 'Παρακαλώ προσθέστε λιγότερους χαρακτήρες';
   return errors;
 }
