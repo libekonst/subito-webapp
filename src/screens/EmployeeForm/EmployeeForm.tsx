3b37ee69e6e374a74581640965e7cd017d164387
AlexLig <ligkalex@gmail.com>
2019-04-25 18:51:55 +0300

    Extract view logic into a different component

--- src/screens/EmployeeForm/EmployeeForm.tsx
+++ src/screens/EmployeeForm/EmployeeForm.tsx
@@ -1,28 +1,14 @@
-import React, { FC, useState, useEffect } from 'react';
-import { withStyles, Theme, createStyles, WithStyles } from '@material-ui/core/styles';
-import WorkIcon from '@material-ui/icons/Work';
-import TextField from '@material-ui/core/TextField';
+import React, { FC, useState } from 'react';
+
 import { addHours, setHours, setMinutes } from 'date-fns';
-import { stickyTopWithAppbar } from '../../styles/mixins';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import AccountIcon from '@material-ui/icons/AccountCircle';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import WorkHourPicker from '../../components/WorkHourPicker';
 import validateOnChange, {
-  shouldType,
   validateOnSubmit,
 } from '../../FormsValidations/employeeFormValidations';
-import { IEmployee } from '../../interfaces';
 import { IEmployeeErrors } from '../../interfaces/IEmployeeErrors';
-import Button from '@material-ui/core/Button';
+import EmployeeFormView from './EmployeeFormView';
 
-interface IProps extends WithStyles<typeof styles> {}
+interface IProps {}
 const EmployeeForm: FC<IProps> = props => {
-  const variant = 'standard';
-  const { classes } = props;
-
   const date: Date = setMinutes(setHours(new Date(), 8), 0);
 
   const [errors, setErrors] = useState<IEmployeeErrors>({
@@ -56,83 +42,19 @@ const EmployeeForm: FC<IProps> = props => {
   };
 
   return (
-    <>
-      <List
-        className={classes.list}
-        subheader={
-          <ListSubheader color="primary" className={classes.listHeader}>
-            Στοιχεία Υπαλλήλου
-          </ListSubheader>
-        }
-      >
-        <ListItem key="employee-fullname">
-          <ListItemIcon>
-            <AccountIcon />
-          </ListItemIcon>
-          <TextField
-            variant={variant}
-            label={errors.name || 'Ονοματεπώνυμο'}
-            error={!!errors.name}
-            value={values.name}
-            onChange={handleChange('name')}
-            className={classes.textField}
-          />
-        </ListItem>
-        <ListItem key="employee-vat">
-          <ListItemIcon>
-            <WorkIcon />
-          </ListItemIcon>
-          <TextField
-            variant={variant}
-            type="tel"
-            label={errors.vat || 'ΑΦΜ'}
-            error={!!errors.vat}
-            value={values.vat}
-            onChange={handleChange('vat')}
-            className={classes.textField}
-          />
-        </ListItem>
-      </List>
-      <List
-        className={classes.list}
-        subheader={
-          <ListSubheader color="primary" className={classes.listHeader}>
-            Τυπικό ωράριο εργασίας
-          </ListSubheader>
-        }
-      >
-        <ListItem key="WorkHour">
-          <WorkHourPicker
-            labelStart="Ώρα Έναρξης"
-            labelFinish="Ώρα Λήξης"
-            valueStart={workStart}
-            valueFinish={workFinish}
-            onChangeStart={setWorkStart}
-            onChangeFinish={setWorkFinish}
-          />
-        </ListItem>
-      </List>
-      <Button onClick={handleSubmit}>ΣΑΜΠ ΜΙΤ</Button>
-    </>
+    <EmployeeFormView
+      {...{
+        errors,
+        values,
+        workStart,
+        workFinish,
+        setWorkStart,
+        setWorkFinish,
+        handleChange,
+        handleSubmit,
+      }}
+    />
   );
 };
 
-const styles = (theme: Theme) =>
-  createStyles({
-    list: {
-      width: '100%',
-      maxWidth: 600,
-      '&:not(:first-child)': {
-        marginTop: '2rem',
-      },
-    },
-    listHeader: {
-      backgroundColor: theme.palette.background.default,
-      ...stickyTopWithAppbar,
-    },
-    textField: {
-      width: '100%',
-    },
-  });
-
-export default withStyles(styles)(EmployeeForm);
+export default EmployeeForm;
---
+++ src/screens/EmployeeForm/EmployeeFormView.tsx
@@ -0,0 +1,119 @@
+import React, { FC } from 'react';
+import { withStyles, Theme, createStyles, WithStyles } from '@material-ui/core/styles';
+import WorkIcon from '@material-ui/icons/Work';
+import TextField from '@material-ui/core/TextField';
+import { stickyTopWithAppbar } from '../../styles/mixins';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import AccountIcon from '@material-ui/icons/AccountCircle';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import WorkHourPicker from '../../components/WorkHourPicker';
+import { IEmployeeErrors } from '../../interfaces/IEmployeeErrors';
+import Button from '@material-ui/core/Button';
+
+
+interface IProps extends WithStyles<typeof styles> {
+  errors: IEmployeeErrors;
+  handleChange: any;
+  values: any;
+  workStart: any;
+  workFinish: any;
+  setWorkStart: any;
+  setWorkFinish: any;
+  handleSubmit: any;
+}
+
+const EmployeeFormView: FC<IProps> = props => {
+  const variant = 'standard';
+  const {
+    classes,
+    errors,
+    handleChange,
+    values,
+    workStart,
+    workFinish,
+    setWorkStart,
+    setWorkFinish,
+    handleSubmit,
+  } = props;
+  return (
+    <>
+      <List
+        className={classes.list}
+        subheader={
+          <ListSubheader color="primary" className={classes.listHeader}>
+            Στοιχεία Υπαλλήλου
+          </ListSubheader>
+        }
+      >
+        <ListItem key="employee-fullname">
+          <ListItemIcon>
+            <AccountIcon />
+          </ListItemIcon>
+          <TextField
+            variant={variant}
+            label={errors.name || 'Ονοματεπώνυμο'}
+            error={!!errors.name}
+            value={values.name}
+            onChange={handleChange('name')}
+            className={classes.textField}
+          />
+        </ListItem>
+        <ListItem key="employee-vat">
+          <ListItemIcon>
+            <WorkIcon />
+          </ListItemIcon>
+          <TextField
+            variant={variant}
+            type="tel"
+            label={errors.vat || 'ΑΦΜ'}
+            error={!!errors.vat}
+            value={values.vat}
+            onChange={handleChange('vat')}
+            className={classes.textField}
+          />
+        </ListItem>
+      </List>
+      <List
+        className={classes.list}
+        subheader={
+          <ListSubheader color="primary" className={classes.listHeader}>
+            Τυπικό ωράριο εργασίας
+          </ListSubheader>
+        }
+      >
+        <ListItem key="WorkHour">
+          <WorkHourPicker
+            labelStart="Ώρα Έναρξης"
+            labelFinish="Ώρα Λήξης"
+            valueStart={workStart}
+            valueFinish={workFinish}
+            onChangeStart={setWorkStart}
+            onChangeFinish={setWorkFinish}
+          />
+        </ListItem>
+      </List>
+      <Button onClick={handleSubmit}>ΣΑΜΠ ΜΙΤ</Button>
+    </>
+  );
+};
+const styles = (theme: Theme) =>
+  createStyles({
+    list: {
+      width: '100%',
+      maxWidth: 600,
+      '&:not(:first-child)': {
+        marginTop: '2rem',
+      },
+    },
+    listHeader: {
+      backgroundColor: theme.palette.background.default,
+      ...stickyTopWithAppbar,
+    },
+    textField: {
+      width: '100%',
+    },
+  });
+
+export default withStyles(styles)(EmployeeFormView);
