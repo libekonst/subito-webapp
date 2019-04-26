a2d8592a6954f99732e5cb6d1feaf0bd5261cb9a
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-04-27 00:12:27 +0300

    Fix validation error importance.

--- src/screens/EmployerForm/validation.ts
+++ src/screens/EmployerForm/validation.ts
@@ -7,23 +7,24 @@ export function validateInput(formValues: IEmployer) {
   const { vat, name, ame, smsNumber } = formValues;
 
   // vat
-  if (!isNumeric.test(vat)) errors.vat = 'Ο ΑΦΜ αποτελείται μόνο απο αριθμούς';
-  if (vat && vat.length !== 9) errors.vat = 'Ο ΑΦΜ αποτελείται από 9 αριθμούς';
   if (!vat) errors.vat = 'Προσθέστε ΑΦΜ';
+  if (vat && !isNumeric.test(vat)) errors.vat = 'Ο ΑΦΜ αποτελείται μόνο απο αριθμούς';
+  if (vat && vat.length !== 9) errors.vat = 'Ο ΑΦΜ αποτελείται από 9 αριθμούς';
 
   // ame
   if (ame && !isNumeric.test(ame)) errors.ame = 'Ο ΑΜΕ αποτελείται μόνο απο αριθμούς';
   if (ame && ame.length !== 10) errors.ame = 'Ο ΑΦΜ αποτελείται από 10 αριθμούς';
 
   // name
-  if (name && name.length > 255) errors.name = 'Προσθέστε λιγότερους χαρακτήρες';
   if (!name) errors.name = 'Προσθέστε ονοματεπώνυμο';
+  if (name && name.length > 255) errors.name = 'Προσθέστε λιγότερους χαρακτήρες';
 
   // smsNumber
-  if (!isNumeric.test(smsNumber)) errors.smsNumber = 'Εισάγετε μόνο αριθμούς';
-  if (smsNumber && smsNumber.length > 10)
-  errors.smsNumber = 'Εισάγετε μέχρι 10 αριθμούς';
   if (!smsNumber) errors.smsNumber = 'Προσθέστε αριθμό αποστολής';
+  if (smsNumber && !isNumeric.test(smsNumber))
+    errors.smsNumber = 'Εισάγετε μόνο αριθμούς';
+  if (smsNumber && smsNumber.length > 10)
+    errors.smsNumber = 'Εισάγετε μέχρι 10 αριθμούς';
 
   return errors;
 }
